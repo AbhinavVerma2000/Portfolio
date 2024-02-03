3018a926525e971bab74e39a9f7310027ced73b5
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-02-04 03:40:03 +0530

    Create _document.js

---
+++ nextfrontend/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en" style={{overflow: "visible"}}>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
