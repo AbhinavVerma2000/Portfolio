2dae5912b518b580a215fa02ebdbc24dc55e23c4
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-02-04 17:08:22 +0530

    Update index.js

--- nextfrontend/pages/index.js
+++ nextfrontend/pages/index.js
@@ -142,7 +142,7 @@ export default function Home({ skills, projects, profile }) {
             <div className="flex flex-col items-center justify-center lg:flex-row">
               <div className="rounded-full border-8 border-primary shadow-xl">
                 <Image
-                  src={builder.image(profile.image).width(200).url()}
+                  src={url(builder.image(profile.image).width(200))}
                   className="h-48 rounded-full sm:h-56"
                   alt="author"
                 />
