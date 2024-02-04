e4eca13cf10858153f12e6d6ee4fbefe897e5f18
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-02-04 09:30:22 +0530

    Update sanity.cli.js

--- sanitybackend/sanity.cli.js
+++ sanitybackend/sanity.cli.js
@@ -3,6 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'spmc989j',
-    dataset: 'production'
+    dataset: 'production',
+    cors: [
+      {
+        "origin": "https://portfolio-fawn-three-23.vercel.app/",
+        "maxAge": 86400 // Adjust as needed
+      }
+    ]
   }
 })
