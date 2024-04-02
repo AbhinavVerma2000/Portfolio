6dd4cfb7ecac2e53b1f1f5b2d2584d7dcfa875a6
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-02 20:51:58 +0530

    Create work.js

---
+++ sanitybackend/schemas/work.js
@@ -0,0 +1,27 @@
+export default {
+    name: 'work',
+    type: 'document',
+    title: 'Work',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+        },
+        {
+            name: 'logo',
+            type: 'image',
+            title: 'Logo'
+        },
+        {
+            name: 'desc',
+            type: 'string',
+            title: 'About',
+        },
+        {
+            name: 'interval',
+            type: 'string',
+            title: 'Time-interval'
+        }
+    ]
+}
