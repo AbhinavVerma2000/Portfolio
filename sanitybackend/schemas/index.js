6bee87fc0e9799a6b9f9d0fcdd25b39886a69474
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-02 20:52:02 +0530

    Update index.js

--- sanitybackend/schemas/index.js
+++ sanitybackend/schemas/index.js
@@ -1,5 +1,6 @@
 import project from "./project";
 import profile from "./profile";
 import know from "./know";
+import work from "./work";
 
-export const schemaTypes = [know, project, profile]
+export const schemaTypes = [know, project, profile, work]
