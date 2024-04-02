e47857f08e7651089ec6bdd99bc87322f065bc0b
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-02 21:07:14 +0530

    Update index.js

--- nextfrontend/pages/index.js
+++ nextfrontend/pages/index.js
@@ -8,7 +8,7 @@ import emailjs from "@emailjs/browser";
 import Link from "next/link";
 
 
-export default function Home({ skills, projects, profile }) {
+export default function Home({ skills, projects, profile, experience }) {
   const client = createClient({
     projectId: "spmc989j",
     dataset: "production",
@@ -353,6 +353,58 @@ export default function Home({ skills, projects, profile }) {
             })}
           </div>
         </div>
+        <div class="container py-16 md:py-20" id="work">
+          <h2 class="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
+            My work experience
+          </h2>
+          <h3 class="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
+            Here&apos;s what I did before freelancing
+          </h3>
+          {experience.map((work)=>{return (<div key={work.title} class="relative mx-auto mt-12 flex w-full flex-col lg:w-2/3">
+            <span class="left-2/5 absolute inset-y-0 ml-10 hidden w-0.5 bg-grey-40 md:block"></span>
+
+            <div class="mt-8 flex flex-col text-center md:flex-row md:text-left">
+              <div class="md:w-2/5">
+                <div class="flex justify-center md:justify-start">
+                  <span class="shrink-0">
+                    <img
+                      src={builder.image(work.logo).url()}
+                      class="h-auto w-32"
+                      alt="company logo"
+                    />
+                  </span>
+                  <div class="relative ml-3 hidden w-full md:block">
+                    <span class="absolute inset-x-0 top-1/2 h-0.5 -translate-y-1/2 transform bg-grey-70"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="md:w-3/5">
+                <div class="relative flex md:pl-18">
+                  <span class="absolute left-8 top-1 hidden h-4 w-4 rounded-full border-2 border-grey-40 bg-white md:block"></span>
+
+                  <div class="mt-1 flex">
+                    <i class="bx bxs-right-arrow hidden text-primary md:block"></i>
+                    <div class="md:-mt-1 md:pl-8">
+                      <span class="block font-body font-bold text-grey-40">
+                        {work.interval}
+                      </span>
+                      <span class="block pt-2 font-header text-xl font-bold uppercase text-primary">
+                        {work.title}
+                      </span>
+                      <div class="pt-2">
+                        <span class="block font-body text-black">
+                          {work.desc}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>)})}
+          
+        </div>
 
         <div
           className="bg-cover bg-top bg-no-repeat pb-16 md:py-16 lg:py-24"
@@ -479,6 +531,9 @@ export async function getServerSideProps(context) {
   const kquery = `*[_type == "knowledge"]`;
   const skills = await client.fetch(kquery);
 
+  const wquery = `*[_type == "work"]`;
+  const experience = await client.fetch(wquery);
+
   const query = `*[_type == "project"]`;
   const projects = await client.fetch(query);
 
@@ -489,6 +544,7 @@ export async function getServerSideProps(context) {
       skills,
       projects,
       profile,
+      experience
     },
   };
 }
