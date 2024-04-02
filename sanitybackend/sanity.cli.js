2a6e3e2388a2b51e171b18ee219de6251bcfd52c
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-04-02 20:52:28 +0530

    Update sanity.cli.js

--- sanitybackend/sanity.cli.js
+++ sanitybackend/sanity.cli.js
@@ -3,12 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'spmc989j',
-    dataset: 'production',
-    cors: [
-      {
-        "origin": "https://portfolio-q1as.vercel.app/",
-        "maxAge": 86400 // Adjust as needed
-      }
-    ]
+    dataset: 'production'
   }
 })
