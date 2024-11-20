e4f8295c12080f376ae7099142bfc8d1866f2ac1
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-11-20 15:03:26 +0530

    Update _document.js

--- nextfrontend/pages/_document.js
+++ nextfrontend/pages/_document.js
@@ -3,7 +3,37 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" style={{overflow: "visible"}}>
-      <Head />
+      <Head>
+      <link
+          crossOrigin="crossorigin"
+          href="https://fonts.gstatic.com"
+          rel="preconnect"
+        />
+
+        <link
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
+          rel="preload"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+
+        {// eslint-disable-next-line @next/next/no-css-tags
+        <link
+          crossOrigin="anonymous"
+          href="/assets/styles/main.min.css"
+          media="screen"
+          rel="stylesheet"
+        />}
+      </Head>
       <body>
         <Main />
         <NextScript />
